df52af2785c019053c6059eca43a9ea16a7758ae
Amedes Júnior <amedes@gmail.com>
2021-06-21 09:30:24 -0300

    Shop -> 21 Jun de 2021 - Aula 213

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+import '../data/dummy_data.dart';
+
+import '../widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
   }
 }
