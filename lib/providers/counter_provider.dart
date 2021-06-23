31a6e32939a207798893d44eaff4e8f842ceb334
Amedes Júnior <amedes@gmail.com>
2021-06-23 09:33:57 -0300

    Shop -> 23 Jun de 2021 - Aula 219

--- lib/providers/counter_provider.dart
+++ lib/providers/counter_provider.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 class CounterState {
   int _value = 1;
 
---
+++ lib/providers/products.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+import '../data/dummy_data.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = DUMMY_PRODUCTS;
+
+  List<Product> get items => [..._items];
+
+  void addProduct(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+}
