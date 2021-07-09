4c570c44aa49e03bd10bf5036aac10659ba1f188
Amedes Júnior <amedes@gmail.com>
2021-07-09 10:27:49 -0300

    Shop -> 09 Jul de 2021 - Aula 258

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -6,11 +6,52 @@ class ProductFormScreen extends StatefulWidget {
 }
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
+  final _priceFocusNote = FocusNode();
+  final _descriptionFocusNote = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocusNote.dispose();
+    _descriptionFocusNote.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Título'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNote);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocusNote,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNote);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                maxLines: 3,
+                focusNode: _descriptionFocusNote,
+                keyboardType: TextInputType.multiline,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
