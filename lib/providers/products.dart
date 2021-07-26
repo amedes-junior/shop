d76f15e27f90a2b5094400c6cd995ff5e7aec0d4
Amedes Júnior <amedes@gmail.com>
2021-07-26 14:17:23 -0300

    Shop -> 26 Jul de 2021 - Aula 277

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,14 +1,15 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import './product.dart';
 import '../data/dummy_data.dart';
 
 class Products with ChangeNotifier {
-
   List<Product> _items = DUMMY_PRODUCTS;
 
-  List<Product> get items  => [ ..._items ];
+  List<Product> get items => [..._items];
 
   int get itemsCount {
     return _items.length;
@@ -19,23 +20,37 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product newProduct) {
-    _items.add(Product(
-      id: Random().nextDouble().toString(),
-      title: newProduct.title,
-      description: newProduct.description,
-      price: newProduct.price,
-      imageUrl: newProduct.imageUrl
-    ));
-    notifyListeners();
+    const url =
+        'https://flutter-shop-e84ba-default-rtdb.firebaseio.com/products.json';
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': newProduct.title,
+        'description': newProduct.description,
+        'price': newProduct.price,
+        'imageUrl': newProduct.imageUrl,
+        'isFavorite': newProduct.isFavorite,
+      }),
+    )
+        .then((response) {
+      _items.add(Product(
+          id: json.decode(response.body)['name'],
+          title: newProduct.title,
+          description: newProduct.description,
+          price: newProduct.price,
+          imageUrl: newProduct.imageUrl));
+      notifyListeners();
+    });
   }
 
   void updateProduct(Product product) {
-    if(product == null || product.id == null) {
+    if (product == null || product.id == null) {
       return;
     }
 
     final index = _items.indexWhere((prod) => prod.id == product.id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items[index] = product;
       notifyListeners();
     }
@@ -43,7 +58,7 @@ class Products with ChangeNotifier {
 
   void deleteProduct(String id) {
     final index = _items.indexWhere((prod) => prod.id == id);
-    if(index >= 0) {
+    if (index >= 0) {
       _items.removeWhere((prod) => prod.id == id);
       notifyListeners();
     }
