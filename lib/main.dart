2cf6b92a7a814786e1e9b0634f6d57ea1029a41c
Amedes Júnior <amedes@gmail.com>
2021-07-28 09:14:19 -0300

    Shop -> 28 Jul de 2021 - Aula 307

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ import './views/cart_screen.dart';
 import './views/orders_screen.dart';
 import './views/products_screen.dart';
 import './views/product_form_screen.dart';
+import './views/auth_screen.dart';
+import './views/auth_home_screen.dart';
 
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import 'package:shop/providers/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,15 +24,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => new Products(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => new Cart(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => new Orders(),
-        ),
+        ChangeNotifierProvider(create: (_) => new Products()),
+        ChangeNotifierProvider(create: (_) => new Cart()),
+        ChangeNotifierProvider(create: (_) => new Orders()),
+        ChangeNotifierProvider(create: (_) => new Auth()),
       ],
       child: MaterialApp(
         title: 'Minha Loja',
@@ -40,7 +38,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: ProductOverviewScreen(),
         routes: {
-          AppRoutes.HOME: (ctx) => ProductOverviewScreen(),
+          AppRoutes.AUTH_HOME: (ctx) => AuthOrHomeScreen(),
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
           AppRoutes.CART: (ctx) => CartScreen(),
           AppRoutes.ORDERS: (ctx) => OrdersScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_token != null &&
+        _expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now())) {
+      return _token;
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> _authenticate(String email, String password, urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDd8Cd66u1u1Q45Ec_xi5-1i-zl4HS_MEI";
+    final response = await http.post(
+      url,
+      body: json.encode({
+        "email": email,
+        "password": password,
+        "returnSecureToken": true,
+      }),
+    );
+
+    final responseBody = json.decode(response.body);
+    if (responseBody['error'] != null) {
+      throw AuthException(responseBody['error']['message']);
+    } else {
+      _token = responseBody['idToken'];
+      _expiryDate = DateTime.now()
+          .add(Duration(seconds: int.parse(responseBody['expiresIn'])));
+      notifyListeners();
+    }
+
+    return Future.value();
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
+  }
+}
